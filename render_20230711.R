f357c45db315d922a454d5908ef9e7bef641a83c
haydento <thayden@usgs.gov>
2023-07-13 16:22:41 -0400

    initial commit

---
+++ render_20230711.R
@@ -0,0 +1,14 @@
+# code to render animation presentation for Iowa.
+
+# 2023-07-10
+library(rmarkdown)
+
+# Renders the site
+render(input = "~/Documents/glatos_workshops/glatos_package_overview/Iowa_glatos_package_overview.Rmd",
+       output_file = "index.html")
+
+
+
+
+
+
